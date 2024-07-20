bd93a15ce3bf7926295ecec68859b573c7ddc401
johnlomat <edwardlomat1503@gmail.com>
2024-07-20 17:17:57 +0800

    Added flowbite react button theme

---
+++ components/themes/flowbite-react/ButtonTheme.tsx
@@ -0,0 +1,13 @@
+import type { CustomFlowbiteTheme } from "flowbite-react";
+
+const ButtonTheme: CustomFlowbiteTheme = {
+  button: {
+    base: "group p-0.5 relative flex items-stretch justify-center",
+    color: {
+      primary:
+        "border border-black bg-neutral-700 font-montserrat font-bold uppercase text-white transition ease-in-out enabled:hover:bg-cyan-700",
+    },
+  },
+};
+
+export default ButtonTheme;
